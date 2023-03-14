1d1348e3fbb537f41dc0ee858e86f11d9a00fcb6
mghlouci <merwan.ghlouci@gmail.com>
2023-03-14 15:52:39 +0100

    ajout exception dans ajouterJoueur

--- src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Joueur_sme/modeles/IServiceJoueur.java
+++ src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Joueur_sme/modeles/IServiceJoueur.java
@@ -1,12 +1,13 @@
 package fr.iut.montreuil.S4_R02_2023_06_QuizzUp_Joueur_sme.modeles;
 
 import fr.iut.montreuil.S4_R02_2023_06_QuizzUp_Joueur_sme.entities.dto.JoueurDTO;
+import fr.iut.montreuil.S4_R02_2023_06_QuizzUp_Joueur_sme.impl.JoueurUniqueException;
 
 import java.util.ArrayList;
 
 public interface IServiceJoueur {
 
-    JoueurDTO ajouterJoueur(String prenom, String pseudo, int anneeNaissance, String centresInterets, String langue);
+    JoueurDTO ajouterJoueur(String prenom, String pseudo, int anneeNaissance, String centresInterets, String langue) throws JoueurUniqueException;
     JoueurDTO supprimerJoueur(JoueurDTO joueur);
     ArrayList<JoueurDTO> listerJoueur();
 
